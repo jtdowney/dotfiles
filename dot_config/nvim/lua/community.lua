c62e1d366dfebec0ab50269bb5f1346be8909851
John Downey <jdowney@gmail.com>
2026-02-11 21:09:32 -0600

    Update astronvim

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -1,3 +1,5 @@
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -5,17 +7,21 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.completion.avante-nvim" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
-  { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+  { import = "astrocommunity.fuzzy-finder.snacks-picker" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.indent.indent-rainbowline" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.elixir-phoenix" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.gleam" },
   { import = "astrocommunity.pack.just" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
@@ -23,6 +29,9 @@ return {
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
   { import = "astrocommunity.search.sad-nvim" },
   { import = "astrocommunity.test.neotest" },
+  -- import/override with your plugins folder
 }
